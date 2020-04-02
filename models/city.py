7a08a5a01c16b3db41494a92981f6be8cb83aa75
salmenz <salmen.zooro@gmail.com>
2020-04-02 19:01:43 +0000

    update with sqlalchamy

--- models/city.py
+++ models/city.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """This is the city class"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel):
@@ -9,5 +12,6 @@ class City(BaseModel):
         state_id: The state id
         name: input name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
