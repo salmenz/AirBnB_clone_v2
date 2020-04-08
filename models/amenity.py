b2c5e16736318f9450aa68796998cb57d775ecd9
Salmen Zouari <salmen.zooro@gmail.com>
2020-04-08 05:46:04 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
