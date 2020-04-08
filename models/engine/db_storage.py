d2bc91e93ac491f8b0431aae665e75b6db76a4ce
salmenz <salmen.zooro@gmail.com>
2020-04-08 02:52:24 +0000

    test

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-"""db_storage engine"""
-import sqlalchemy
+"""This is the DB storage class for AirBnB"""
 import os
 from models.base_model import BaseModel, Base
-from models.user import User
-from models.amenity import Amenity
-from models.review import Review
 from models.state import State
 from models.city import City
 from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import (create_engine)
 
 
+
 class DBStorage():
-    """class"""
+    """ 
+    class
+    """
     __engine = None
     __session = None
-    classes = ['Amenity',
-               'City',
-               'User',
-               'Review',
-               'State',
-               'Place']
+    all_classes = ['Amenity',
+                   'City',
+                   'Review',
+                   'Place',
+                   'User',
+                   'State']
 
-    def __init__(self):
+    d def __init__(self):
         """init"""
         self.__engine = create_engine
         ('mysql+mysqldb://{}:{}@{}/{}'.format
@@ -32,52 +34,43 @@ class DBStorage():
           os.getenv("HBNB_MYSQL_PWD"),
           os.getenv("HBNB_MYSQL_HOST"),
           os.getenv("HBNB_MYSQL_DB")),
-         pool_pre_ping=True)
-        if os.getenv("HBNB_ENV") == "test":
+         pool_pre_ping=True)                                                               if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """
-        return a dictionary: (like FileStorage)
-        """
-        d = {}
-        objects = list()
-        if cls:
-            objects = self.__session.query(eval(cls)).all()
-            for item in objects:
-                key = item.__class__.__name__ + '.' + item.id
-                d[key] = item
+        """ Returns a dictionary of objects which their name is specified """
+        mylist = list()
+        newdict = dict()
+        if cls is not None:
+            obj = eval(cls)
+            mylist = self.__session.query(obj).all()
+            for item in mylist:
+                key = item.__class__.__name__ + "." + item.id
+                newdict[key] = item
         else:
-            for item in self.classes:
-                item = eval(item)
-                objects = self.__session.query(item).all()
-                for obj in objects:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    d[key] = obj
-        return d
+            for obj in self.all_classes:
+                obj = eval(obj)
+                mylist = self.__session.query(obj).all()
+                for item in mylist:
+                    key = item.__class__.__name__ + "." + item.id
+                    newdict[key] = item
+        return (newdict)
 
     def new(self, obj):
-        """
-        add the object to the current database session (self.__session)
-        """
+        """ Adds a new object to the database """
         self.__session.add(obj)
 
     def save(self):
-        """
-        commit all changes of the current database session (self.__session)
-        """
+        """ Commits all changes made in current session """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        Delete obj from current database session if not None
-        """
+        """ Deletes an existing object from the database """
         if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """ Creates all tables in database and
-        create the current database session """
+        """ Creates all tables in database and set a session """
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(expire_on_commit=False)
         session_factory.configure(bind=self.__engine)
